37187e4168f37f27235935dd4570fc774438c280
Jason Dillaman <dillaman@redhat.com>
2015-09-23 18:10:25 -0400

    cls_rbd: new methods for handling mirroring
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

---
+++ src/cls/rbd/cls_rbd_types.h
@@ -0,0 +1,51 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#ifndef CEPH_CLS_RBD_TYPES_H
+#define CEPH_CLS_RBD_TYPES_H
+
+#include "include/int_types.h"
+#include "include/buffer.h"
+#include "include/encoding.h"
+#include <iosfwd>
+#include <string>
+
+namespace ceph { class Formatter; }
+
+namespace cls {
+namespace rbd {
+
+struct MirrorPeer {
+  MirrorPeer() : pool_id(-1) {
+  }
+  MirrorPeer(const std::string &cluster_uuid, const std::string &cluster_name,
+             int64_t pool_id, const std::string &client_name)
+    : cluster_uuid(cluster_uuid), cluster_name(cluster_name), pool_id(pool_id),
+      client_name(client_name) {
+  }
+
+  std::string cluster_uuid;
+  std::string cluster_name;
+  int64_t pool_id;
+  std::string client_name;
+
+  void encode(bufferlist &bl) const;
+  void decode(bufferlist::iterator &it);
+  void dump(Formatter *f) const;
+
+  static void generate_test_instances(std::list<MirrorPeer*> &o);
+
+  bool operator==(const MirrorPeer &rhs) const;
+};
+
+std::ostream& operator<<(std::ostream& os, const MirrorPeer& peer);
+
+WRITE_CLASS_ENCODER(MirrorPeer);
+
+} // namespace rbd
+} // namespace cls
+
+using cls::rbd::encode;
+using cls::rbd::decode;
+
+#endif // CEPH_CLS_RBD_TYPES_H
