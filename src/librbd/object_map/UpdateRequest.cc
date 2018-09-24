7ab1504cdf3d9e6a9cd3f32a3c55bd24cd6dfa76
Jason Dillaman <dillaman@redhat.com>
2018-09-24 14:45:09 -0400

    librbd: do not invalidate object map if update races with copyup
    The copyup state machine needs to iterate over all object maps to update
    the existence for the object. If an snapshot is being removed concurrently,
    it's possible to invalidate the object map for the image.
    
    Fixes: http://tracker.ceph.com/issues/24516
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/librbd/object_map/UpdateRequest.cc
+++ src/librbd/object_map/UpdateRequest.cc
@@ -72,6 +72,15 @@ template <typename I>
 void UpdateRequest<I>::handle_update_object_map(int r) {
   ldout(m_image_ctx.cct, 20) << "r=" << r << dendl;
 
+  if (r == -ENOENT && m_ignore_enoent) {
+    // ignore potential race with deleting a snapshot when copyup
+    // races with a snapshot removal
+    r = 0;
+  }
+  if (r < 0 && m_ret_val == 0) {
+    m_ret_val = r;
+  }
+
   {
     RWLock::RLocker snap_locker(m_image_ctx.snap_lock);
     RWLock::WLocker object_map_locker(m_image_ctx.object_map_lock);
@@ -85,7 +94,7 @@ void UpdateRequest<I>::handle_update_object_map(int r) {
   }
 
   // no more batch updates to send
-  complete(r);
+  complete(m_ret_val);
 }
 
 template <typename I>
